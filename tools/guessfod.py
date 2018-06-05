beaa761b0431d0cb04bd15c6908005eb01cd9a4d
jackyzy823 <jackyzy823@gmail.com>
2018-06-05 16:33:14 +0800

    use socket and head to do faster

--- tools/guessfod.py
+++ tools/guessfod.py
@@ -1,18 +1,46 @@
-import requests
+# import requests
 import string
 from itertools import product
 import time
-print "start ",time.time()
+import random
+start = time.time()
+print "start ",start
+
+import socket
+
+
 
 from multiprocessing.dummy import Pool
-s =requests.session()#for reuse http connection 
+
+# s =requests.session()#for reuse http connection
+# adapter = requests.adapters.HTTPAdapter(pool_connections=SIZE, pool_maxsize=SIZE)
+# s.mount('http://', adapter)
+
+p = None
+
 #use http replace https to reduce traffic
 def worker(a):
-    if s.get('%s%s%s%s.mp4.m3u8'%a).status_code!=404:
+    if s.head('%s%s%s%s.mp4.m3u8'%a).status_code!=404:
         print '%s%s%s%s.mp4.m3u8'%a
         print "find ",time.time()
 
-p = Pool(50)
-res = p.imap(worker,product(string.lowercase+string.digits,repeat=4))
+def worker2(a):
+		addr = ["202.247.51.61","202.247.51.62","202.247.51.70","202.247.51.71"][(ord(a[0])+ord(a[1]))%4]
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.connect((addr, 80))
+		for v in product(string.lowercase+string.digits,repeat=2):
+			s.sendall("HEAD /www08/fod-plus7/phls/video/01234/4f07/4f07810006me113%s%s%s%s.mp4.m3u8 HTTP/1.1\r\nConnection:Keep-Alive\r\nHost:136ea006abc838d0070ae4bde485da88.cdnext.stream.ne.jp\r\n\r\n"%(a[0],a[1],v[0],v[1]))
+			data = s.recv(1024)
+			if data[9:12] != '404':
+				print "%s%s%s%s"%(a[0],a[1],v[0],v[1])
+				print "end ",time.time()
+				print "dur ",time.time() - start
+				p.close()
+				p.terminate()
+		s.close()
+
+
+p = Pool(36*36)
+res = p.imap_unordered(worker2,product(string.lowercase+string.digits,repeat=2))
 for i in res:
     pass
