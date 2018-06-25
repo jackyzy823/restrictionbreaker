32d301964cde2cc2d74b1bf8ba0a78320f39e671
jackyzy823 <jackyzy823@gmail.com>
2018-06-25 23:17:14 +0800

    new method for fod ;add support for tvasahi

--- webextension/background.js
+++ webextension/background.js
@@ -9,23 +9,23 @@ chrome.webRequest.onBeforeSendHeaders.addListener(function(req){
 },
 {urls:["*://*.api.brightcove.com/playback/*","*://abematv.akamaized.net/region*","*://linear-abematv.akamaized.net/*"]},["blocking","requestHeaders"])
 
+chrome.webRequest.onBeforeRequest.addListener(function(req){
+  return {cancel:true}
+},{urls:["http://www.tv-asahi.co.jp/douga/common/js/checkWithin.js"]},["blocking"])
+// asahi can also redirect  within_area_check.php to dataurl
+
+
 //for firefox 
 // change response to -> 
 // <RESULT>
 // <FLAG TYPE="true"/>
 // </RESULT>
 
-// do not work under iframe?
-chrome.webRequest.onBeforeSendHeaders.addListener(function(req){
-  console.log("in fuji fod!!!")
-  //block geocontrol1. for fod-geo
-  //add cookie to domain i.fod....
-  // let it retry itself
-  
-  // samesite --> from chrome51
+// cookie may set after 3 times retry. may force to cancel slowly?
+// new solution use redirect to data url
+// double ensurence! 
+chrome.webRequest.onBeforeRequest.addListener(function(req){
   chrome.cookies.set({url:"https://i.fod.fujitv.co.jp/",domain:"i.fod.fujitv.co.jp",name:"geo",value:"true",httpOnly:false,expirationDate: Date.now()/1000.0+3600000},function(res){
-  console.log(res);
   });//need fujitv.co.jp permission in manifest
-  return {cancel:true};
-
+  return {redirectUrl: "data:text/xml,<RESULT> <FLAG TYPE=\"true\"/> </RESULT>"};
 },{urls:["*://geocontrol1.stream.ne.jp/fod-geo/check.xml*"]},["blocking"])
