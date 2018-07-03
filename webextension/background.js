fd8ecfc30835e946825217be4c0858d5754eff4d
jackyzy823 <jackyzy823@gmail.com>
2018-07-03 16:25:31 +0800

    know why firefox do not work

--- webextension/background.js
+++ webextension/background.js
@@ -3,7 +3,7 @@ console.log("Your IP:"+CHOSENIP+",You can change it by reload the extension if s
 
 
 chrome.webRequest.onBeforeSendHeaders.addListener(function(req){
-  console.log("why firefox do not go into here?")
+  console.log("why firefox do not go into here? "+req.url);
   req.requestHeaders = req.requestHeaders.filter(function(x){
     return x.name.toLowerCase()!= 'x-forwarded-for';
   });
@@ -46,12 +46,50 @@ if(chrome.webRequest.filterResponseData){
   },{urls:["*://*.api.brightcove.com/playback/*"]},["blocking"]);
 }
 else{
+  // inspect and monkey patch xhr? https://www.moesif.com/blog/technical/apirequest/How-We-Captured-AJAX-Requests-with-a-Chrome-Extension/
+  ////does not work because it needs resposeheaders for statistics ,but redirecturl cannot provide
+  // const BCOV_POLICY = {
+  //   //YTV
+  //   "5330942432001":"BCpkADawqM0kGrWxZoXJvJj5Uv6Lypjp4Nrwzz1ktDAuEbD1r_pj0oR1900CRG04FFkxo0ikc1_KmAlB4uvq_GnFwF4IsG_v9jhYOMajC9MkdVQ-QrpboS7vFV8RvK20V5v-St5WGPfXotPx",
+  //   //TX
+  //   "3971130137001":"BCpkADawqM1F2YPxbuFJzWtohXjxdgDgIJcsnWacQKaAuaf0gyu8yxCQUlca9Dh7V0Uu_8Rt5JUWZTpgcqzD_IT5hRVde8JIR7r1UYR73ne8S9iLSroqTOA2P-jtl2EUw_OrSMAtenvuaXRF",
+  //   //TBS
+  //   "4031511847001":"BCpkADawqM1n_azNkrwm-kl2UhijTLt4W7KZ6KS9HluAoLPvyRFu2X4Xu2dUuW-lLOmc6X7WjsiBwh83m8ecNmxl-pVy9w3M9iI6-en-_wIDvNJixpoMf4BhdOPtwO_7XIol9P3wVrq2BIzw",
+  //   "4394098881001":"BCpkADawqM3m-3484dphPL5raj3jQJVlFecOYAvpxhtJaK99BVRKtxd9SC6q0kOsknI1FD3kplVUaJzneAQb55EkCcDHrD9m_yoesmjsIfJpKQXJKfmQ5LfAFJnmf2Sv48heP_R1PGznwbAn",
+  //   //NTV
+  //   "4394098882001":"BCpkADawqM1s6XkqRoC2a0eEORY7FFF780eHkHQZ93Fw752A9swymrSMZEVF1d7G3mSby3Etzj8MGJp_ZwXpbSTH1ApfZxZ1FSPQ4LXDQhpaMRADtCbxKFTpAxGYwN61DYKKksmg4uwcdhLD",
+  //   //MBS
+  //   "5102072605001":"BCpkADawqM1VhDl0FtgrrM8jB-hVNkcrdrx4x9C_60OSeN4jIHynGkIKw0PY1cOsRqQYJOnJRscPAbdPTcpzZ_4g89Gcte_yQFW-yeWxzrPECulIh9ZlaZsJ_3rH7Gjs_RnuWHx_lTzilaxh",
+  //   //KTV
+  //   "5718741494001":"BCpkADawqM1llDtMelQ9nQyE91bAc-E5T1B0135MCCRZ_o4FlDkGWQY8t8Nrt1fJKAgui-kLefX-JGaRItrDXh_C1GlIgCSv-rhNPQYKJsY8nZp_IoJ38Mf3B5BSJLFulW0QhgQduipc9j4D",
+  //   //EX no publisherid
+  //   //ABC   
+  //   "5102072603001":"BCpkADawqM2NfzEA47jZiJNK0SYahFenNwAtoehfrIAaCqxmHjBidnt_YfvFnp5j-Zi58FPj-zXAHATYU1nnOOuEf9XXV8JRGYSuZ5dgyNc2RjGv2Ej5zGfhxWs3_p4F7huxtbAD9fzQlg7b",
+  //   //World cup
+  //   "5764318572001":"BCpkADawqM3KJLCLszoqY9KsoXN2Mz52LwKx4UXYRuEaUGr-o3JBSHmz_0WRicxowBj8vmbGRK_R7Us96DdBYuYEoVX9nHJ3DjkVW5-8L6bRmm6gck8IaeLLw21sM6mOHtNs9pIJPF6a4qSZlO6t_RlkpMY6sasaIaSYlarJ_8PFMPdxxfY6cGtJDnc"
+  // }
+  // chrome.webRequest.onBeforeRequest.addListener(function(req){
+  //   let publisherid = /accounts\/(\d*?)\/videos/.exec(req.url)[1];
+  //   let policykey = BCOV_POLICY[publisherid];
+  //   let myReq = new XMLHttpRequest();
+  //   myReq.open('GET',req.url,false); 
+  //   myReq.setRequestHeader('X-Forwarded-For',CHOSENIP);
+  //   myReq.setRequestHeader("Accept","application/json;pk="+policykey);
+  //   myReq.send();
+  //   let json = myReq.response;
+  //   json.cue_points = [];
+  //   return {redirectUrl: "data:text/json;"+JSON.stringify(json)};
+
+  // },{urls:["*://*.api.brightcove.com/playback/*"]},["blocking"]);
   //onbeforerequest -> redirect to xhr response (with x-forwarded-for and accept policy [get policy from publisherid]) the modify json
 }
 
 
+//OPTIONS for data url do not allow Access-Control-Request-Headers: x-requested-with
 //preview
 chrome.webRequest.onBeforeRequest.addListener(function(req){
+  //not always correct
+  //example: wanz00768 -> service tkwanz768 -> wanz00768
   let cid = /cid=(.*?)\//.exec(req.url)[1];
   if(cid.substr(-5,2) == "00"){
     cid = cid.substr(0,cid.length-5)+ cid.slice(-3);
@@ -66,18 +104,15 @@ chrome.webRequest.onBeforeRequest.addListener(function(req){
 },{urls:["*://www.dmm.co.jp/service/-/html5_player/*"]},["blocking"])
 
 
-/*
-<iframe type="text/html" src="http://www.dmm.co.jp/service/-/html5_player/=/cid=1star788/mtype=AhRVShI_/service=digital/floor=videoa/mode=/" id="DMMSample_player_now" width="560" height="440" scrolling="no" border="0" style="border:none;" frameborder="0" allowfullscreen="" allow="autoplay"></iframe>
-*/
-
-
-// FAILED BECAUSE OF CORS (OPTIONS)
-// may do this via javascript ?
+// has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.
+// FAILED BECAUSE OF CORS (OPTIONS) what if req.method ==get ? (failed too)
+//may do this via javascript ?
 // chrome.webRequest.onBeforeRequest.addListener(function(req){
-//   console.log("force replace!!!!")
-//   return {redirectUrl:req.url.replace(/\/\d+\/playlist.m3u8/,'/1080/playlist.m3u8')}
-
-// },{urls:["*://linear-abematv.akamaized.net/channel/*/playlist.m3u8*"]},["blocking"])
+//   if(req.method.toLowerCase()=='get'){
+//     console.log("force replace!!!!")
+//     return {redirectUrl:req.url.replace(/\/\d+\/playlist.m3u8/,'/1080/playlist.m3u8')}
+//   }
+// },{urls:["*://linear-abematv.akamaized.net/channel/*/*/playlist.m3u8*"]},["blocking"])
 /*
 Abema resolution
 https://linear-abematv.akamaized.net/channel/anime-live/720/playlist.m3u8
@@ -107,3 +142,6 @@ chrome.webRequest.onBeforeRequest.addListener(function(req){
   });//need fujitv.co.jp permission in manifest
   return {redirectUrl: "data:text/xml,<RESULT> <FLAG TYPE=\"true\"/> </RESULT>"};
 },{urls:["*://geocontrol1.stream.ne.jp/fod-geo/check.xml*"]},["blocking"])
+
+
+//TODO: for FOD and firefox , ua -> chrome to avoid flash
